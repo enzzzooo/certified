7fad7e3eb02a1da78eec172b85a827662b2818dd
Enzo <enzzzooo@proton.me>
2025-12-01 10:16:44 +0000

    gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { DeployButton } from "@/components/deploy-button";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -10,9 +9,14 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+          <div className="w-full max-w-7xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold text-2xl">
-              <Link href={"/"}>Certified</Link>
+              <Link
+                href={"/"}
+                className="bg-[linear-gradient(90deg,#ef4444,#facc15,#3b82f6,#ec4899)] bg-clip-text text-transparent"
+              >
+                Certified
+              </Link>
             </div>
             <Suspense>
               <AuthButton />
@@ -26,18 +30,8 @@ export default function Home() {
           </main>
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-8">
+          Certified &copy; 2025
           <ThemeSwitcher />
         </footer>
       </div>
